4adc909398bf04dbe0b2be26c2614dde8901d31f
lmislm <lmjustlm@163.com>
2017-12-31 17:39:34 +0800

    用户数异步更新

--- client.js
+++ client.js
@@ -18,7 +18,23 @@ new Vue({
     created: function () { //who join,est
         //如果server emits '用户已加入’，需要更新连接用户的数组
         socket.on('user joined',function (socketId) {//如果获得则
-            this.connectUsers.push(socketId); //更新
+            //先获取已经连接的用户
+            axios.get('/onlineusers')
+                .then(function (response) {
+                    for(var key in response.data){
+                        if(this.connectUsers.indexOf(key) <= -1){
+                            this.connectUsers.push(key); //更新
+                        }
+                    }
+                    console.log(this.connectUsers);
+                }.bind(this));
+        }.bind(this));
+        //如果server broadcasts 'user left'，从已连接数组中删除用户
+        socket.on('user left',function (socketId) {
+            var index = this.connectUsers.indexOf(socketId);  //很重要，同一窗口的用户刷新人数不变
+            if(index >= 0){
+                this.connectUsers.splice(index,1);
+            }
         }.bind(this));
     },
     method:{
--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/',function (request,response) {
     response.sendFile(__dirname + '/index.html');
 });
 
+app.get('/onlineusers',function (request, response) {
+   // console.log(io.sockets.adapter.rooms);
+   // response.send(io.nsps['/'].adapter.rooms);//fix the bug of socket.io 1.x.x ,
+    response.send(io.sockets.adapter.rooms);
+});
+
 io.on('connection',function (socket) {
     console.log('一个用户已经连接' + socket.id);
 
@@ -23,5 +29,7 @@ io.on('connection',function (socket) {
 
     socket.on('disconnect',function () {
         console.log('用户离开' + socket.id);
+        //告诉所有clients用户已经离开
+        socket.broadcast.emit('user left',socket.id);
     });
 });
